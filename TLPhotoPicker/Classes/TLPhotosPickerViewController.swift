9c8945c17d9dad8d291325e1956c7233dde3aa48
tilltue <tilltue@gmail.com>
2017-12-13 01:34:40 +0900

    scale up image

--- TLPhotoPicker/Classes/TLPhotosPickerViewController.swift
+++ TLPhotoPicker/Classes/TLPhotosPickerViewController.swift
@@ -213,8 +213,9 @@ extension TLPhotosPickerViewController {
         guard let layout = self.collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
         let count = CGFloat(self.configure.numberOfColumn)
         let width = (self.view.frame.size.width-(5*(count-1)))/count
-        self.thumbnailSize = CGSize(width: width, height: width)
-        layout.itemSize = self.thumbnailSize
+        let scale = UIScreen.main.scale
+        self.thumbnailSize = CGSize(width: width*scale, height: width*scale)
+        layout.itemSize = CGSize(width: width, height: width)
         self.collectionView.collectionViewLayout = layout
         self.placeholderThumbnail = centerAtRect(image: self.configure.placeholderIcon, rect: CGRect(x: 0, y: 0, width: width, height: width))
         self.cameraImage = centerAtRect(image: self.configure.cameraIcon, rect: CGRect(x: 0, y: 0, width: width, height: width), bgColor: self.configure.cameraBgColor)
