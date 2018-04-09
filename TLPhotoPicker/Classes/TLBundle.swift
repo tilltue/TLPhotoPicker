7b37f9f89290dcc78d2087eca75df8777f87df0e
wade.hawk <junhyi.park@gmail.com>
2018-04-09 22:36:58 +0900

    change bundle name

--- TLPhotoPicker/Classes/TLBundle.swift
+++ TLPhotoPicker/Classes/TLBundle.swift
@@ -9,9 +9,17 @@
 import Foundation
 
 class TLBundle {
+    class func resourceBundle(classCoder: AnyClass) -> Bundle {
+        let bundle = Bundle(for: classCoder)
+        if let url = bundle.url(forResource: "TLPhotoPicker", withExtension: "bundle") {
+            return Bundle(url: url)!
+        }else {
+            return bundle
+        }
+    }
     class func podBundleImage(named: String) -> UIImage? {
         let podBundle = Bundle(for: TLBundle.self)
-        if let url = podBundle.url(forResource: "TLPhotoPickerController", withExtension: "bundle") {
+        if let url = podBundle.url(forResource: "TLPhotoPickerImage", withExtension: "bundle") {
             let bundle = Bundle(url: url)
             return UIImage(named: named, in: bundle, compatibleWith: nil)!
         }
