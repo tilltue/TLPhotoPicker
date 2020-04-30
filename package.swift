1854596ec2e01577ab129057f1667e1f3c11c3b8
Maxwell Ainatchi <maxwell.ainatchi@gmail.com>
2020-04-30 16:30:35 -0400

    Added a package.swift file for SwiftPM support

---
+++ package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "TLPhotoPicker",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "TLPhotoPicker",
+            targets: ["TLPhotoPicker"]),
+    ],
+    dependencies: [
+        // no dependencies
+    ],
+    targets: [
+        .target(
+            name: "TLPhotoPicker",
+            dependencies: [])
+    ]
+)
