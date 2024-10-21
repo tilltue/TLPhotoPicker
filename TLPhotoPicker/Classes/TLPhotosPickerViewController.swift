89f5114ca64188c1bedbbcd8621300e48a71ca4e
nitishkumar1632 <nitish.kumar16@flipkart.com>
2024-10-21 17:18:49 +0530

    Fixed image upload breaking issue

--- TLPhotoPicker/Classes/TLPhotosPickerViewController.swift
+++ TLPhotoPicker/Classes/TLPhotosPickerViewController.swift
@@ -287,7 +287,8 @@ extension TLPhotosPickerViewController {
             return
         }
         let count = CGFloat(self.configure.numberOfColumn)
-        let width = (self.view.frame.size.width-(5*(count-1)))/count
+        //width is coming in decimal, adding floor will remove the decimal and give us integer value. 
+        let width = floor((self.view.frame.size.width-(5*(count-1)))/count)
         self.thumbnailSize = CGSize(width: width, height: width)
         layout.itemSize = self.thumbnailSize
         self.collectionView.collectionViewLayout = layout
