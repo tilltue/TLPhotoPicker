c92bd70ee85899d64bfb88865165f6d227fdc988
imoroz <imoroz@themeetgroup.com>
2025-08-12 22:48:00 +0200

    Fix photo permissions image view and bugs

--- TLPhotoPicker/Classes/TLPhotosPickerViewController.swift
+++ TLPhotoPicker/Classes/TLPhotosPickerViewController.swift
@@ -388,11 +388,23 @@ open class TLPhotosPickerViewController: UIViewController {
 
     private func checkAuthorization() {
         if #available(iOS 14.0, *) {
-            let status = PHPhotoLibrary.authorizationStatus(for:  .readWrite)
-            processAuthorization(status: status)
+            let status = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+            if status == .notDetermined {
+                PHPhotoLibrary.requestAuthorization(for: .readWrite) { newStatus in
+                    self.processAuthorization(status: newStatus)
+                }
+            } else {
+                processAuthorization(status: status)
+            }
         } else {
             let status = PHPhotoLibrary.authorizationStatus()
-            processAuthorization(status: status)
+            if status == .notDetermined {
+                PHPhotoLibrary.requestAuthorization { newStatus in
+                    self.processAuthorization(status: newStatus)
+                }
+            } else {
+                processAuthorization(status: status)
+            }
         }
     }
 
